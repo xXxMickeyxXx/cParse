cebedaa86112591421e487d759c5a27ef42d5270
Mickey Drury <drurymickey@gmail.com>
2025-11-09 01:33:13 -0500

    Updated code in 'main.c' source file for both example and testing sub-packages to have it work with the API design (i.e., via the 'include/cParse.h' lib header file) (2025-11-09)

--- examples/arithmeticLang/main.c
+++ examples/arithmeticLang/main.c
@@ -1,12 +1,23 @@
-#include "../../source/common.h"
-#include "../../source/utils.h"
+#include <cParse.h>
+#include <stdio.h>
+#include <string.h>
 
 
-static int __EXIT_CODE__ = 0;
+#define PRINT(text) printf("%s\n", text); \
+	fflush(stdout)
 
 
 int main(int argc, char* argv[]) {
-	ASSERT(__EXIT_CODE__ == 0, "ERROR!!!");
-	PRINT("HELLO FROM 'arithmeticLang'!!!");
-	return __EXIT_CODE__;
+	// const char* _test_parser_id = "arithmeticLang";
+	int _test_parser_id_int = 104;
+	int* _test_parser_id;
+	_test_parser_id = &_test_parser_id_int;
+	*_test_parser_id = 8675309;
+
+
+	cParser* _new_test_parser = NEW_cParser(_test_parser_id);
+	PRINT("\n");
+	printf("PARSER ID ---> %d", *cParser_ID(_new_test_parser, int*));
+	PRINT("\n");
+	return 0;
 }
--- testing/main.c
+++ testing/main.c
@@ -1,12 +1,18 @@
-#include "../source/common.h"
-#include "../source/utils.h"
+// #include "../include/cParse.h"
+#include <cParse.h>
 
 
 static int __EXIT_CODE__ = 0;
 
 
+void display_parser_id(const char* parser_id) {
+	cParser* _new_parser = NEW_cParser(parser_id);
+	PRINT(FORMAT_STRING("TEST PARSER ID ---> '%s'", cParser_ID(_new_parser)));
+	__free_cParser__(_new_parser);
+}
+
+
 int main(int argc, char* argv[]) {
-	ASSERT(__EXIT_CODE__ == 0, "ERROR!!!");
-	PRINT("HELLO FROM 'testing'!!!");
-	return __EXIT_CODE__;
+	display_parser_id("<<* -TEST_PARSER_ID- •>>");
+	return 0;
 }
