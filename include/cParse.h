348e577fe56770c0d9d9021c5dabf734dae7b9a4
Mickey Drury <drurymickey@gmail.com>
2025-11-11 18:13:15 -0500

    Updated API macro's to better ensure desired outcome when using them (2025-11-11)

--- include/cParse.h
+++ include/cParse.h
@@ -5,7 +5,7 @@
 
 // @NOTE: macros for handling the lifetime of 'cParser' instance
 #define NEW_cParser(parser_id) (__new_cParser__((void*)(parser_id)))
-#define FREE_cParser(self) (__free_cParser__(self))
+#define FREE_cParser(self) (__free_cParser__((self)))
 
 // @NOTE: conveinance macros for 'cParser'
-#define cParser_ID(self, type) ((type)(self->parser_id))
+#define cParser_ID(self) ((self)->parser_id)
