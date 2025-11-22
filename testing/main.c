871c83cff2ad0da592be1202044a6505b014e3ef
Mickey Drury <drurymickey@gmail.com>
2025-11-22 06:45:23 -0500

    Added 'testing_utils.h' to the 'testing' dir to add centralize some common code into it's own header file; defined a type representing a test case, i.e., 'TEST_CASE_t' (takes no arguments and doesn't return a value) (2025-11-22)

--- testing/main.c
+++ testing/main.c
@@ -1,52 +1,66 @@
 #include <string.h>
 
 #include <cParse.h>
-#include "../utils/common.h"
-#include "../utils/utils.h"
+#include "testing_utils.h"
 
 
-#define TOTAL_TEST_COUNT (2)
-#define TEST_PASSED(test_number) printf("\n\n\t[-"); \
-		printf("\033[38;5;10mTEST \033[0m"); \
-		printf("\033[38;5;11m%d/%d\033[0m", test_number, TOTAL_TEST_COUNT); \
-		printf("\033[38;5;10m PASSED\033[0m"); \
-		printf("-]\n")
-#define TEST_FAILED(test_number) printf("\n\n\t[-"); \
-		printf("\033[38;5;9mTEST \033[0m"); \
-		printf("\033[38;5;11m%d/%d\033[0m", test_number, TOTAL_TEST_COUNT); \
-		printf("\033[38;5;9m FAILED\033[0m"); \
-		printf("-]\n")
+#define TEST_CASE_COUNT (3)
 
 
 void test_1(void) {
-	cParser* _new_test_parser = NEW_cParser("HELLO MOTO!!!");
-
 	PRINT("\n");
-	if (strcmp((char*)cParser_ID(_new_test_parser), "HELLO MOTO!!!") == 0) {
-		TEST_PASSED(1);
-	} else {
-		TEST_FAILED(1);
-	}
+	DISPLAY_TEST_RESULT(1, 1, TEST_CASE_COUNT);
 	PRINT("\n");
 }
 
 
 void test_2(void) {
-	cParser* _new_test_parser = NEW_cParser((&((int){1001})));
+	cParser* _new_test_parser = NEW_cParser("HELLO MOTO!!!");
+
+	// PRINT("\n");
+	// if (strcmp((char*)cParser_ID(_new_test_parser), "HELLO MOTO!!!") == 0) {
+	// 	TEST_PASSED2(1);
+	// } else {
+	// 	TEST_FAILED2(1);
+	// }
+	// PRINT("\n");
 
 	PRINT("\n");
-	if ((*(int*)cParser_ID(_new_test_parser)) == 1001) {
-		TEST_PASSED(2);
-	} else {
-		TEST_FAILED(2);
-	}
+	bool _result = (strcmp((char*)cParser_ID(_new_test_parser), "HELLO MOTO!!!") == 0);
+	DISPLAY_TEST_RESULT(_result, 2, TEST_CASE_COUNT);
+	PRINT("\n");
+}
+
+
+void test_3(void) {
+	cParser* _new_test_parser = NEW_cParser((&((int){1001})));
+
+	// PRINT("\n");
+	// if ((*(int*)cParser_ID(_new_test_parser)) == 1001) {
+	// 	TEST_PASSED2(2);
+	// } else {
+	// 	TEST_FAILED2(2);
+	// }
+	// PRINT("\n");
+
+	PRINT("\n");
+	bool _result = ((*(int*)cParser_ID(_new_test_parser)) == 1001);
+	DISPLAY_TEST_RESULT(_result, 3, TEST_CASE_COUNT);
 	PRINT("\n");
 }
 
 
 int main(int argc, char* argv[]) {
-	test_1();
-	PRINT("\n");
-	test_2();
+	// @NOTE: 'TEST_CASE_t' ointer array for executing all added
+	TEST_CASE_t TEST_CASES[TEST_CASE_COUNT] = {
+		test_1,
+		test_2,
+		test_3
+	};
+	
+	for (int i = 0; i < TEST_CASE_COUNT; i++) {
+		TEST_CASES[i]();
+		PRINT("\n");
+	}
 	return 0;
 }
---
+++ testing/testing_utils.h
@@ -0,0 +1,33 @@
+#ifndef CPARSE_TESTING_UTILS_H
+#define CPARSE_TESTING_UTILS_H
+
+
+// @NOTE: Helper macros for outputting if a specific test, out of total number
+//		  of tests, passed or fail; uses color
+#define TEST_PASSED(test_number, total_tests) printf("\n\n\t[-"); \
+		printf("\033[38;5;10mTEST \033[0m"); \
+		printf("\033[38;5;11m%d/%d\033[0m", test_number, total_tests); \
+		printf("\033[38;5;10m PASSED\033[0m"); \
+		printf("-]\n")
+#define TEST_FAILED(test_number, total_tests) printf("\n\n\t[-"); \
+		printf("\033[38;5;9mTEST \033[0m"); \
+		printf("\033[38;5;11m%d/%d\033[0m", test_number, total_tests); \
+		printf("\033[38;5;9m FAILED\033[0m"); \
+		printf("-]\n")
+#define DISPLAY_TEST_RESULT(condition, test_number, total_tests) (__display_test_result((condition), (test_number), (total_tests)))
+
+
+// @NOTE: 'TEST_CASE_t' is a function pointer type for test cases
+typedef void (*TEST_CASE_t)(void);
+
+
+static inline void __display_test_result(bool condition, int test_number, int total_tests) {
+	if (condition) {
+		TEST_PASSED(test_number, total_tests);
+	} else {
+		TEST_FAILED(test_number, total_tests);
+	}
+}
+
+
+#endif
