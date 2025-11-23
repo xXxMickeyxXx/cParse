a09a09cfca3f74f8e294f7a2e306985aa7251793
Mickey Drury <drurymickey@gmail.com>
2025-11-22 22:24:55 -0500

    Attempted to test SIMD intrinsics however it didn't work right away and to avoid spending forever messing around with it, committing what I have so that I can maybe add to it later, but also so I can continue on with the more important test cases (2025-11-22)

--- testing/main.c
+++ testing/main.c
@@ -1,10 +1,14 @@
 #include <string.h>
 
 #include <cParse.h>
+#include "../source/parser.h"
+#include "../source/scanner.h"
 #include "testing_utils.h"
 
+#include "testing_SIMD_matmul.h"
 
-#define TEST_CASE_COUNT (3)
+
+#define TEST_CASE_COUNT (4)
 
 
 void test_scanner(void) {
@@ -13,9 +17,27 @@ void test_scanner(void) {
 }
 
 
+void test_creating_new_parser(void) {
+	cParser* new_parser = NEW_cParser((&((long){8675309})));
+
+	bool new_parser_allocated_condition = new_parser != NULL;
+	DISPLAY_TEST_RESULT(new_parser_allocated_condition, 2, TEST_CASE_COUNT);
+}
+
+
+void test_freeing_parser(void) {
+	cParser* new_parser = NEW_cParser((&((long){18001233345})));
+	FREE_cParser(new_parser);
+	new_parser = NULL;
+
+	bool new_parser_free_condition = new_parser == NULL;
+	DISPLAY_TEST_RESULT(new_parser_free_condition, 3, TEST_CASE_COUNT);
+}
+
+
 void test_parser(void) {
 	bool test_case_condition = FALSE;
-	DISPLAY_TEST_RESULT(test_case_condition, 1, TEST_CASE_COUNT);
+	DISPLAY_TEST_RESULT(test_case_condition, 4, TEST_CASE_COUNT);
 }
 
 
@@ -23,6 +45,8 @@ int main(int argc, char* argv[]) {
 	// @NOTE: 'TEST_CASE_t' ointer array for executing all added
 	TEST_CASE_t TEST_CASES[TEST_CASE_COUNT] = {
 		test_scanner,
+		test_creating_new_parser,
+		test_freeing_parser,
 		test_parser
 	};
 	
