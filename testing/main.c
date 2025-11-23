d7ad48f6c4f669fa513a530ffee5fdee2266d916
Mickey Drury <drurymickey@gmail.com>
2025-11-22 23:08:14 -0500

    Updating 'testing' branch in order to keep separation of concerns as I'm currently working on the 'source/tensor' branch and code currently (2025-11-22)

--- testing/main.c
+++ testing/main.c
@@ -2,6 +2,7 @@
 
 #include <cParse.h>
 #include "testing_utils.h"
+#include "../source/tensor.h"
 
 
 #define TEST_CASE_COUNT (3)
@@ -13,9 +14,18 @@ void test_scanner(void) {
 }
 
 
-void test_parser(void) {
+void test_cParser(void) {
 	bool test_case_condition = FALSE;
-	DISPLAY_TEST_RESULT(test_case_condition, 1, TEST_CASE_COUNT);
+	DISPLAY_TEST_RESULT(test_case_condition, 2, TEST_CASE_COUNT);
+}
+
+
+void test_Tensor_new_and_free(void) {
+	size_t example_tensor_shape[4] = {1, 1, 4, 4};
+	Tensor* new_test_tensor = NEW_Tensor(4, example_tensor_shape, sizeof(uint64_t));
+
+	bool test_case_condition = FREE_Tensor(new_test_tensor) >= 0;
+	DISPLAY_TEST_RESULT(test_case_condition, 3, TEST_CASE_COUNT);
 }
 
 
@@ -23,7 +33,8 @@ int main(int argc, char* argv[]) {
 	// @NOTE: 'TEST_CASE_t' ointer array for executing all added
 	TEST_CASE_t TEST_CASES[TEST_CASE_COUNT] = {
 		test_scanner,
-		test_parser
+		test_cParser,
+		test_Tensor_new_and_free
 	};
 	
 	for (int i = 0; i < TEST_CASE_COUNT; i++) {
