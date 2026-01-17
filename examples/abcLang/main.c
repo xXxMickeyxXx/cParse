cf6ee6391d56e66ac01b30f90e911376af7384af
Mickey Drury <drurymickey@gmail.com>
2026-01-17 06:57:59 -0500

    Save checkpoint; continuing on with designing and building lib by implementing an example language, which will require resolving both ambiguity and context sensitivity (2026-01-17)

--- examples/abcLang/main.c
+++ examples/abcLang/main.c
@@ -6,10 +6,13 @@
 #include "../../utils/utils.h"
 
 
+#define COLOR_STRING(offset, text) (FORMAT_STRING("\033[38;5;%dm%s\033[0m", offset, text))
+
+
 int main(int argc, char* argv[]) {
 	PRINT("\n");
 
-	const char* _example_input_1_path = "/Users/mickey/Desktop/C_Lang/libs/cparse/examples/abcLang/example_input/abcLang_example_input_1.txt";
+	const char* _example_input_1_path = "/Users/mickey/Desktop/C_Lang/libs/cparse/examples/abcLang/example_input/abcLang_example_input.txt";
 	FILE* _example_input_1_read = fopen(_example_input_1_path, "r");
 
 	int _example_input_1_char;
@@ -17,25 +20,15 @@ int main(int argc, char* argv[]) {
 		PRINT("ERROR READING EXAMPLE INPUT # 1");
 		exit(-1);
 	}
+
+	char _input_buff_1[100];
+	size_t i = 0;
 	while ((_example_input_1_char = fgetc(_example_input_1_read)) != EOF) {
-		printf("%c",(char)_example_input_1_char);
+		_input_buff_1[i++] = _example_input_1_char;
 	}
+	_input_buff_1[i] = NULL_CHAR;
 	fclose(_example_input_1_read);
-
-	const char* _example_input_2_path = "/Users/mickey/Desktop/C_Lang/libs/cparse/examples/abcLang/example_input/abcLang_example_input_2.txt";
-	
-	int _example_input_2_char;
-	FILE* _example_input_2_read = fopen(_example_input_2_path, "r");
-	if (_example_input_2_read == NULL) {
-		PRINT("ERROR READING EXAMPLE INPUT # 2");
-		exit(-1);
-	}
-	while ((_example_input_2_char = fgetc(_example_input_2_read)) != EOF) {
-		printf("%c",(char)_example_input_2_char);
-	}
-	fclose(_example_input_2_read);
-
-
+	PRINT(FORMAT_STRING("%s\n\n", COLOR_STRING(10, _input_buff_1)));
 
 
 	// const char* _parser_ID = "-HELLO_MOTO-";
@@ -50,7 +43,6 @@ int main(int argc, char* argv[]) {
 	// FREE_GRAMMAR(abcLang_grammar);
 	// FREE_GRAPH(_parse_graph);
 
-	PRINT("\n");
-	PRINT("\n");
+
 	return 0;
 }
---
+++ source/cursor.h
@@ -0,0 +1,3 @@
+#ifndef CPARSE_CURSOR_H
+#define CPARSE_CURSOR_H
+#endif
